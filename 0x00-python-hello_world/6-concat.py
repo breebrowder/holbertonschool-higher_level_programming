971befdd2019e19801cc319d25aa7b152f0d56f1
breezybrow <browder.bree@gmail.com>
2021-04-26 21:51:35 +0000

    Concatenating two strings

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = str1 + " " + str2 # concatentating both together and adding space
+str3 = str1 + " " + str2
 print("Welcome to {}!".format(str3))

27a46679b8598557408fe11e6b71d9becabdd33a
breezybrow <browder.bree@gmail.com>
2021-04-28 21:10:46 +0000

    Handling multiples of 3 and 5

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for digit in range(1, 101):
+        if (digit % 3 == 0):
+            print("Fizz", end=" ")
+
+        elif (digit % 5 == 0):
+            print("Buzz", end=" ")
+
+        elif (digit % 3 == 0) and (digit % 5 == 0):
+            print("FizzBuzz", end=" ")
+
+        else:
+            print((digit), end=" ")

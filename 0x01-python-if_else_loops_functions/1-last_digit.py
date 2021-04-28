7c06710bcaa3dccc7091ebc61aff45244791d91f
breezybrow <browder.bree@gmail.com>
2021-04-28 19:32:40 +0000

    Fixing Pep8 errors and spacing issues

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,17 +3,17 @@ import random
 number = random.randint(-10000, 10000)
 
 if number > 0:
-    lastdigit = number % 10
+    last = number % 10
 elif number < 0:
-    lastdigit = number % -10
+    last = number % -10
 else:
-    lastdigit = number % 10
+    last = number % 10
 
-if (lastdigit > 5):
-    print("Last digit of {} is {} and is greater than 5".format(number, lastdigit))
+if (last > 5):
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
 
-elif (lastdigit == 0):
-    print("Last digit of {} is {} and is 0".format(number, lastdigit))
+elif (last == 0):
+    print("Last digit of {} is {} and is 0".format(number, last))
 
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastdigit))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))

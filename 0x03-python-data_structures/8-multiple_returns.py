a923bd8903ac00d7f271f294256ab9295c833288
breezybrow <browder.bree@gmail.com>
2021-04-29 04:01:47 +0000

    Returning tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if len(sentence) > 0:
         return tuple([len(sentence), sentence[0]])
     else:
-        return tuple([None])
+        return tuple([0, None])

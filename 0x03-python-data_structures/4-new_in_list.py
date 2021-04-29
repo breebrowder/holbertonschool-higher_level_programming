0c8df3bba0d5f64b7760285add437f687fae2b80
breezybrow <browder.bree@gmail.com>
2021-04-29 04:21:07 +0000

    Fixing errors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if my_list is None:
         return
     cpylist = my_list[:]
-    if idx >= 0 < len(cpylist):
+    if 0 <= idx < len(cpylist):
         cpylist[idx] = element
     return cpylist

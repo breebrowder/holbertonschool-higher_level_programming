e158359b67c27d5cf5aa120b5874022162ede0fd
breezybrow <browder.bree@gmail.com>
2021-04-29 22:25:07 +0000

    Switching statements

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,14 +2,14 @@
 def fizzbuzz():
     for digit in range(1, 101):
 
+        if (digit % 3 == 0) and (digit % 5 == 0):
+            print("FizzBuzz", end=" ")
+
         if (digit % 3 == 0):
             print("Fizz", end=" ")
 
         elif (digit % 5 == 0):
             print("Buzz", end=" ")
-
-        elif (digit % 3 == 0) and (digit % 5 == 0):
-            print("FizzBuzz", end=" ")
 
         else:
             print((digit), end=" ")

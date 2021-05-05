686080895b3fb294f786fddc36fd88ca9ef028d4
breezybrow <browder.bree@gmail.com>
2021-05-05 00:54:52 +0000

    Using private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Square that will define a square """
+
+
+class Square:
+    """ HEAD SQUARE """
+    def __init__(self, size):
+        """ Init size variable """
+        self.__size = size

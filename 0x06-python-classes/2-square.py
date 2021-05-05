b4f15f5b98f92a30290ef7cc7130a135d041e116
breezybrow <browder.bree@gmail.com>
2021-05-05 01:00:03 +0000

    Size Validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Square that defines a square """
+
+
+class Square:
+    """ HEAD SQUARE """
+    def __init__(self, size=0):
+        """ Init size attribute """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")

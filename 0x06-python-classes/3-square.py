612d48cf5ac6555c200ccf3ca244f2f0606cda00
breezybrow <browder.bree@gmail.com>
2021-05-05 04:59:15 +0000

    PEP8 not done

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A square that defines a square by: private instance attribute: size,
+instantation with optional size, raising TypeError or ValueError, and
+public instance method that returns square area """
+
+
+class Square:
+      """ a class that initializes a function within a class: self is the
+      reference to the new object being created """
+      def __init__(self, size=0):
+
+            """ Init of private size attribute """
+            self.__size = size
+
+            if not (isinstance(size, int)):
+                  raise TypeError("size must be an integer")
+
+                  if (size < 0):
+                        raise ValueError("size must be >= 0")
+
+      def area(self):
+
+            squared_x2 = self.__size * self.__size
+            return(squared_x2)

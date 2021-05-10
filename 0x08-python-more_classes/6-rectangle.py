05a0a3695c5e3b52247d685640db2901b600ae74
breezybrow <browder.bree@gmail.com>
2021-05-10 05:26:12 +0000

    Adding public class attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,6 +14,7 @@ class Rectangle:
         instance as the first arg: self; self handles all instances """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):

7139854777be29f980b6e08785334cd6b9313487
breezybrow <browder.bree@gmail.com>
2021-05-10 05:33:05 +0000

    Adding public class attribute init to #

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
 
     number_of_instances = 0
 
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """ Initializing method within a class that will recieve the

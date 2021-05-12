62d8623846988dc35c6a95bba02f8ec9a48c9a29
breezybrow <browder.bree@gmail.com>
2021-05-12 02:43:46 +0000

    Fixing errors, pep8, Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Module defining class: write a class that inherits from previous task """
-
+""" Module that will define our class """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Instantiation with width and height """
+
+    """Class Rectangle inherited from previous task 7 """
+
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """Instantiation of width and height"""
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)

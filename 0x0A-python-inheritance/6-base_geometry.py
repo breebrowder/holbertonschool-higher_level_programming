78629871e5286b98db4592937000e217b7b25170
breezybrow <browder.bree@gmail.com>
2021-05-12 02:06:59 +0000

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" write a class based on previous task """
+
+
+class BaseGeometry:
+    """ raise an exception with a message """
+    def area(self):
+
+        raise Exception("area() is not implemented")

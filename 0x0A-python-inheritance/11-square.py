4b9b72ecdb1309f61a2929a1066a3be086a31cdd
breezybrow <browder.bree@gmail.com>
2021-05-12 04:42:18 +0000

    Task 11, all mandatory, just like with Rect

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" write a class that inherits another class """
+""" Write a class that inherits another class """
 
 Rectangle = __import__('9-rectangle').Rectangle
 
@@ -13,7 +13,7 @@ class Square(Rectangle):
         self.__size = size
 
     def area(self):
-        return self.__width * self.__height
+        return self.__size * self.__size
 
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)

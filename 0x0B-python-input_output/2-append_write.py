45d39bdc958e9323089a9791c67d995756f6651f
breezybrow <browder.bree@gmail.com>
2021-05-12 19:59:04 +0000

    Changing mode to open text file for appending

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" function that appends str at the end of txt file (UTF8) &  returns chars """
+""" function that appends str at end of txt file (UTF8) &  returns chars """
 
 
 def append_write(filename="", text=""):
-   """ using with statement to simplify management of our files """
-
-   with open(filename, mode="a", encoding="utf-8") as myFile:
+    """ using with statement to simplify management of our files """
+    with open(filename, mode="a", encoding="utf-8") as myFile:
         addedchars = myFile.write(text)
         return addedchars

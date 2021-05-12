44e16c9a29db2d7b0a7aee31ea3991df505aceaa
breezybrow <browder.bree@gmail.com>
2021-05-12 04:26:38 +0000

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/in/python3
+""" write a class that inherits another class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Instantiation with constructor """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        self.__width = size
+        self.__height = size
+
+    def area(self):
+        return self.__width * self.__height

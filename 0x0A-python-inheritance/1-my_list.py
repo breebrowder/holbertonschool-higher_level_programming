36d0ab799315f6826853af2c456deeceaa3341a3
breezybrow <browder.bree@gmail.com>
2021-05-12 00:06:50 +0000

    Still need to add tests

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class MyList that inherits from list """
+
+
+class MyList(list):
+    """ add public instance method that prints the list, but sorted """
+
+    def print_sorted(self):
+        print(sorted(self))

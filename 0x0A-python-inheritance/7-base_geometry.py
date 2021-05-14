ab2d03746eb3e0c3acba31cef1d02d7023c6c53d
breezybrow <browder.bree@gmail.com>
2021-05-14 21:36:38 +0000

    Second deadline, missing test txt file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-""" write a class based on previous task """
+""" Write a class based on Task 6 """
 
 
 class BaseGeometry:
-    """ raise an exception with a message """
+    """ Public instance method that raises an exception """
     def area(self):
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if isinstance(value, int) is False:
+        if not type(value) == int:
             raise TypeError("{} must be an integer".format(name))
 
-        elif (value <= 0):
-            raise ValueError("{} must be  greater than 0".format(name))
+        if (value <= 0):
+            raise ValueError("{} must be greater than 0".format(name))

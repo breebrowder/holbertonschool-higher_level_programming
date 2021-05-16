785b025c3830af8e368f337cb7930d208aa4114d
breezybrow <browder.bree@gmail.com>
2021-05-16 22:45:23 +0000

    Creating class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Write the first class Base """
+
+
+import json
+
+
+class Base:
+
+    __nb_objects = 0
+    """ Private class attribute """
+
+    def __init__(self, id=None):
+        """ Instantiation """
+        if id is not None:
+            self.id = id
+        else:
+            """ Increment objects & assign new value to public inst attr id """
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

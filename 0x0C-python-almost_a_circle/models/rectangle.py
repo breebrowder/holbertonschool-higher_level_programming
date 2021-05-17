ae0160fa3c52959c94ee9461778ac0eb911ac599
breezybrow <browder.bree@gmail.com>
2021-05-17 13:55:05 +0000

    Pushing new updates to 0x0C

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,40 +1,121 @@
 #!/usr/bin/python3
-""" Writing a class Rectangle that inherits from Base """
+"""This module is for Rectangle that inherits from Base"""
 from models.base import Base
 
 
 class Rectangle(Base):
 
-    """ Class constructor below """
+    """This is the base for the rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Private instance attr; each with own getter and setter """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
         if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-
-        if (width <= 0):
-            raise ValueError("width must be > 0")
-
+            raise TypeError('width must be an integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
         if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-
-        if (height <= 0):
-            raise ValueError("height must be > 0")
-
+            raise TypeError('height must be an integer')
+        if height <= 0:
+            raise ValueError('height must be > 0')
         if not isinstance(x, int):
-            raise TypeError("x must be an integer")
-
-        if (x < 0):
-            raise ValueError("x must be >= 0")
-
+            raise TypeError('x must be an integer')
+        if x < 0:
+            raise ValueError('x must be >= 0')
         if not isinstance(y, int):
-            raise TypeError("y must be an integer")
+            raise TypeError('y must be an integer')
+        if y < 0:
+            raise ValueError('y must be >= 0')
 
-        if (y < 0):
-            raise ValueError("y must be >= 0")
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
+
+    def area(self):
+        """This is to find the area of the rectangle"""
+        return self.__height * self.__width
+
+    def display(self):
+        """Prints in stdout the rectangle with #"""
+        if self.width == 0 or self.height == 0:
+            return ("")
+        rect = ""
+        for yspaces in range(self.y):
+            print()
+        for i in range(self.height):
+            for xspaces in range(self.x):
+                rect += " "
+            for j in range(self.width):
+                rect += "#"
+            if i != self.height - 1:
+                rect += ("\n")
+        print(rect)
+
+    def update(self, *args):
+        """Replacing items with updated args"""
+        if len(args):
+            for i, e in enumerate(args):
+                if i == 0:
+                    self.id = e
+                if i == 1:
+                    self.width = e
+                if i == 2:
+                    self.height = e
+                if i == 3:
+                    self.x = e
+                if i == 4:
+                    self.y = e
+
+    def __str__(self):
+        """Returning in format [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+                                                       self.width, self.height)

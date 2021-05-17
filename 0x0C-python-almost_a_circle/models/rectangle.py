d3d85604c0ac6e6c70cb29bebcf00058d92fdbd5
breezybrow <browder.bree@gmail.com>
2021-05-17 14:45:08 +0000

    New changes to 0x0C, pep8 compliant

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
 
-    """Class Rectangle; constructor defining private instance attributes """
+    """Class Rectangle with Base """
 
     def __init__(self, width, height, x=0, y=0, id=None):
 

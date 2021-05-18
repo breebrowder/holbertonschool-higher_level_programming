521ff498c343c700e61abedc6f763bd4a30fdcc3
breezybrow <browder.bree@gmail.com>
2021-05-18 03:50:47 +0000

    Adding first classmethod to Base, dealing with string repr, pep8 compliant

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,19 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """ Returning list of the JSON string repr """
         if json_string is None:
             return "[]"
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file """
+        filename = cls.__name__ + ".json"
+        reprList = []
+        if list_objs is not None:
+            for i in list_objs:
+                reprList.append(cls.to_dictionary(i))
+        with open(filename, mode="w", encoding="utf-8") as myFile:
+            myFile.write(cls.to_json_string(reprList))

71dbdbcfd294fb30f93354322028cd78a9b66a1c
breezybrow <browder.bree@gmail.com>
2021-05-18 04:40:33 +0000

    Importing module, working last task, still some errors, pep8 compliant

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ Class Base used to manage id attribute in all future classes """
 
 import json
+from os import path
 
 
 class Base:
@@ -46,9 +47,26 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """ Returning instance with attr set; using update to create dummy """
         if cls.__name__ == "Rectangle":
             dummyList = cls(6, 6)
         if cls.__name__ == "Square":
             dummyList = cls(6)
         dummyList.update(**dictionary)
         return dummyList
+
+    @classmethod
+    def load_from_file(cls):
+        """ Updating class by returning list of special instances """
+        filename = cls.__name__ + ".json"
+        instances = []
+
+        if path.exists(filename) is False:
+            return []
+        if path.exists(filename):
+            with open(filename, mode="r", encoding="utf-8") as myFile:
+                f = myFile.read()
+                List = cls.from_json_string(f)
+            for dict in List:
+                instances.append(cls.create(**dict))
+                return instances

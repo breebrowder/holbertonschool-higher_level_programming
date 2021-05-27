782f8b75d618f7980343995b7981db5cb40b003d
breezybrow <browder.bree@gmail.com>
2021-05-27 22:36:16 +0000

    Similar to previous

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Create the table unique_id on your MySQL server.
-CREATE TABLE unique_id (id INT DEFUALT 1 UNIQUE, name VARCHAR (256));
+CREATE TABLE unique_id (id INT DEFUALT 1 UNIQUE, name VARCHAR(256));

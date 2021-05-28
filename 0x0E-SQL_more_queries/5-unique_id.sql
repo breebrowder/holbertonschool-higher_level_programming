acd2d18ee8479e5f4fc4f9db397ba4658dc0e20a
breezybrow <browder.bree@gmail.com>
2021-05-28 01:48:06 +0000

    Why won't this pass the checks?!

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,5 @@
 -- Create the table unique_id on your MySQL server.
 -- ID INT with default value 1 must be unique
-CREATE TABLE unique_id (id INT DEFUALT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE unique_id (
+       id INT DEFUALT 1 UNIQUE,
+       name VARCHAR(256));

24dd8dd9a5a9e314130c34a81324ef9cdbe860d5
breezybrow <browder.bree@gmail.com>
2021-06-02 03:44:02 +0000

    Filter states- adding one line of code to 0

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Write a script that lists states with name starting with N from database """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * from states ORDER BY states.id")
+    columns = cur.fetchall()
+    for column in columns:
+        if row[1].startswith("N"):
+            print(column)
+
+    # close all cursors
+    cur.close()
+
+    # close all databases
+    db.close()

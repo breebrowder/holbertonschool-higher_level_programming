a4412d3e9ff7ce4fd276e3525ea350d688e62bb9
breezybrow <browder.bree@gmail.com>
2021-06-02 22:46:57 +0000

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const first = 'C is fun\n';
+const second = 'Python is cool\n';
+const third = 'JavaScript is amazing';
+
+const sentence = first + second + third;
+
+console.log(sentence);

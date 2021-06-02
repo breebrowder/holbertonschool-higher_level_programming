ac0aa911b8e52c5fbde98f8ddfcbae8b673160cf
breezybrow <browder.bree@gmail.com>
2021-06-02 03:50:27 +0000

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Write a script that takes in an arg & displays all values in the states """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * from states WHERE name LIKE BINARY\
+    "{}" ORDER BY states.id ASC".format(sys.argv[4]))
+
+    columns = cur.fetchall()
+    for column in columns:
+        if column[1].startswith("N"):
+            print(column)
+
+    # close all cursors
+    cur.close()
+
+    # close all databases
+    db.close()

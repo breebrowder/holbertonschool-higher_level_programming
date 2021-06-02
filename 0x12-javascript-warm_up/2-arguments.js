e54697c607296bf9b75526245ef959c6af172025
breezybrow <browder.bree@gmail.com>
2021-06-02 23:56:52 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const argLength = process.argv.length - 2;
 if (argLength === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argLength === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }

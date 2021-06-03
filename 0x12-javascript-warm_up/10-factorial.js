7878789fda7a571c41a7c52ead90aa4624a67c56
breezybrow <browder.bree@gmail.com>
2021-06-03 22:28:36 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,6 +3,6 @@
 const numero = parseInt(process.argv[2]);
 
 function factorial (n) {
-  return n === 1 || isNaN(n) ? 1 : factorial(n -1) * n;
+  return n === 1 || isNaN(n) ? 1 : factorial(n - 1) * n;
 }
 console.log(factorial(numero));

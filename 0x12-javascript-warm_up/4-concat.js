35180901b6b198d30d88e5e6ab7d1586624b0b3f
breezybrow <browder.bree@gmail.com>
2021-06-03 02:38:34 +0000

    Fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-const argLength = process.argv.length;
-const firstArg = process.argv[2];
-const secondArg = process.argv[3];
-
-argLength <= 2 || argLength >= 5 ? console.log('undefined is undefined') : argLength === 3 ? console.log('${firstArg}' + ' is undefined') : console.log('${firstArg}' + ' is ' + '${secondArg}');
+console.log(process.argv[2] + ' is ' + process.argv[3]);

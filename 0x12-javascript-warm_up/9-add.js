9d538be936023f2c62df8945b7131ed8799c9956
breezybrow <browder.bree@gmail.com>
2021-06-03 22:27:00 +0000

    Add-will come back for last tasks in proj

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const firstInt = process.argv[2];
-const secondInt = process.argv[3];
+const secInt = process.argv[3];
 function add (a, b) {
   return (parseInt(a) + parseInt(b));
 }
-console.log(add(firstInt, secondInt));
+console.log(add(firstInt, secInt));

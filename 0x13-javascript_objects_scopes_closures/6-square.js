26b477f8309bcc5cbf52ed8e9b2f332b5dcf51ba
breezybrow <browder.bree@gmail.com>
2021-06-04 03:53:48 +0000

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const AlphaSquare = require('./5-square');
+
+class Square extends AlphaSquare {
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.width; i++) {
+       console.log(`${c}`.repeat(this.height));
+      }
+     } else {
+       this.print();
+     }
+  }
+}
+module.exports = Square;

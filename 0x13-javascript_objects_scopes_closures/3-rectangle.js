3a81867333318bd1565cc0863dfe43f2422c74ba
breezybrow <browder.bree@gmail.com>
2021-06-04 03:34:54 +0000

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,10 +3,9 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+     this.width = w;
+     this.height = h;
+   }
 }
    print () {
      const x = 'X';

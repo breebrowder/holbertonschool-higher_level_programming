d6d135cf47341e9c46ea844bbd31ec895ec46ae9
breezybrow <browder.bree@gmail.com>
2021-06-07 19:08:32 +0000

    First State

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Print the first State object from the database hbtn_0e_6_usa """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    tmp = 'mysql+mysqldb://{}:{}@localhost/{}'\
+          .format(argv[1], argv[2], argv[3])
+    engine = create_engine(tmp)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    instance = session.query(State).order_by(State.id).first()
+
+    try:
+        print("{}:{}".format(instance.id, instance.name))
+    except BaseException:
+        print("Nothing")
+    session.close()

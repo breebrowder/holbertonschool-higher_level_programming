0fb77725038a3925f5fdadcf9caf30508c1ec3f7
breezybrow <browder.bree@gmail.com>
2021-06-07 22:30:03 +0000

    Occurences-1,4,2

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let total = 0;
-    for (const i in list) {
-	if (list[i] === searchElement) {
-	    total++;
-	}
+  let total = 0;
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      total++;
     }
-    return (total);
+  }
+  return (total);
 };

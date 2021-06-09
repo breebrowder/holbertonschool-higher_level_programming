344ce0114ebee2f690189660cd0a2f0f1d748acf
breezybrow <browder.bree@gmail.com>
2021-06-09 20:10:38 +0000

    Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Write class that prevents user from dynamically creating new instance """
+
+class LockedCLass:
+    """ LockedClass is our class """
+__slots__ = "first_name"

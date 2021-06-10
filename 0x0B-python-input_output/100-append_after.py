36ea9bdc904b5d3d293e0a4bd93cd7666e577e8f
breezybrow <browder.bree@gmail.com>
2021-06-10 22:11:48 +0000

    Adv-Search and Update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Insert a line of text to a file, after each line containing a spec str """
+
+def append_after(filename="", search_string="", new_string=""):
+    text = ""
+    with open(filename) as myFile:
+        for lineoftext in myFile:
+            text += lineoftext
+            if search_string in lineoftext:
+                text += new_string
+    with open(filename, mode="w", encoding="utf-8") as w_perm:
+        w_perm.write(text)
